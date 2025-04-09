6f5fad722b4da328d4240a6a652ef2f6bff3a3fe
JoshUndefined <joshundefined.tv@gmail.com>
2025-04-09 08:17:56 -0700

    Add main script

---
+++ main.py
@@ -0,0 +1,17 @@
+# TODO
+
+# Parse argument for stream ID or default to latest stream
+
+
+# Get list of stream markers and stream details from Twitch API
+
+
+# Download all stream markers, length based on marker name set from Deckboard
+
+
+# Download chat history
+
+
+# Update database with stream info and chat history
+
+
